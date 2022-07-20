8cd0be67176c40f621eaf2e7139bc819015f2648
berit-stange <76659950+berit-stange@users.noreply.github.com>
2022-07-20 18:46:48 +0200

    added toggle class function

---
+++ js/accordion.js
@@ -0,0 +1,24 @@
+function showAnswer1() {
+    var answer = document.getElementById("hide1");
+    answer.classList.toggle("show-answer");
+}
+
+function showAnswer2() {
+    var answer = document.getElementById("hide2");
+    answer.classList.toggle("show-answer");
+}
+
+function showAnswer3() {
+    var answer = document.getElementById("hide3");
+    answer.classList.toggle("show-answer");
+}
+
+function showAnswer4() {
+    var answer = document.getElementById("hide4");
+    answer.classList.toggle("show-answer");
+}
+
+function showAnswer5() {
+    var answer = document.getElementById("hide5");
+    answer.classList.toggle("show-answer");
+}
